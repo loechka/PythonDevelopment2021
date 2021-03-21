30097a94d8b6bf286943d1969a123fbe27746f3c
loechka <anuta.anna.ann@yandex.ru>
2021-03-21 18:36:52 +0300

    template for the game

---
+++ game15.py
@@ -0,0 +1,25 @@
+import time
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+        self.showtime()
+
+    def showtime(self):
+        self.time.set(time.strftime("%c"))
+
+    def createWidgets(self):
+        self.time = tk.StringVar()
+        self.timeButton = tk.Button(self, text='Time', command=self.showtime)
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.timeLabel = tk.Label(self, textvariable=self.time)
+        self.timeButton.grid()
+        self.quitButton.grid(row=0, column=1)
+        self.timeLabel.grid(columnspan=2)
+
+app = Application()
+app.master.title('GAME 15')
+app.mainloop()

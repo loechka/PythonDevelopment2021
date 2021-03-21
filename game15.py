9777b5e3de78fc7ffafef5b5877caae52b34762f
loechka <anuta.anna.ann@yandex.ru>
2021-03-21 19:23:50 +0300

    creating canvas and widgets

--- game15.py
+++ game15.py
@@ -1,24 +1,43 @@
-import time
 import tkinter as tk
+import random
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
-        self.grid()
+        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+        self.winfo_toplevel().geometry('300x400+300+100')
         self.createWidgets()
-        self.showtime()
-
-    def showtime(self):
-        self.time.set(time.strftime("%c"))
 
     def createWidgets(self):
-        self.time = tk.StringVar()
-        self.timeButton = tk.Button(self, text='Time', command=self.showtime)
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.timeLabel = tk.Label(self, textvariable=self.time)
-        self.timeButton.grid()
-        self.quitButton.grid(row=0, column=1)
-        self.timeLabel.grid(columnspan=2)
+        BOARD_SIZE = 4
+        TOP_WIDTH, TOP_HEIGHT = 10, 4
+
+        top=self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        for i in range(BOARD_SIZE):
+            self.rowconfigure(i, weight=1)
+            self.columnconfigure(i, weight=1)
+        self.rowconfigure(BOARD_SIZE, weight=1)
+        # menu
+        self.NewButton = tk.Button(self, text='New', command = self.restart, width = TOP_WIDTH, height = TOP_HEIGHT)
+        self.ExitButton = tk.Button(self, text='Exit', command = self.quit, width = TOP_WIDTH, height = TOP_HEIGHT)
+
+        self.ExitButton.grid(row = 0, column = 2, columnspan = 2)
+        self.NewButton.grid(row = 0, column = 0, columnspan = 2)
+        # buttons
+        self.buttons, self.numbers = list(), list(range(15))
+        random.shuffle(self.numbers)
+
+        for i in range(len(self.numbers)):
+            number = self.numbers[i]
+            self.buttons.append(tk.Button(self, text=str(number+1)))
+            self.buttons[-1].grid(row=i//4+1, column=i%4, sticky="SEWN")
+
+    def restart(self):
+        self.destroy()
+        self.__init__()
+
 
 app = Application()
 app.master.title('GAME 15')
